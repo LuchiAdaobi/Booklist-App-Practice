965843976e496cd54923d4def519753f4947ed13
LuchiAdaobi <Luchichronicles@gmail.com>
2022-03-09 17:43:50 +0100

    add and remove book from local storage

--- main.js
+++ main.js
@@ -88,7 +88,7 @@ class Store {
   static addBook(book) {
     const books = Store.getBooks();
     books.push(book);
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
   }
 
   static removeBook(isbn) {
